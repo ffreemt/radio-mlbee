d63b04303a5823f4de2f50d972597ad845c05638
freemt <yucongo+fmt@gmail.com>
2022-06-12 09:25:27 +0800

    first commit

---
+++ tests/test_radio_mlbee.py
@@ -0,0 +1,17 @@
+"""Test radio_mlbee."""
+# pylint: disable=broad-except
+from radio_mlbee import __version__
+from radio_mlbee import radio_mlbee
+
+
+def test_version():
+    """Test version."""
+    assert __version__[:3] == "0.1"
+
+
+def test_sanity():
+    """Check sanity."""
+    try:
+        assert not radio_mlbee()
+    except Exception:
+        assert True
