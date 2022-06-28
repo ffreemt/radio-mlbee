31349515ded09621c718dfc91ea511a24de9e254
freemt <yucongo+fmt@gmail.com>
2022-06-28 13:37:01 +0800

    Fix utils.py

--- radio_mlbee/utils.py
+++ radio_mlbee/utils.py
@@ -6,9 +6,9 @@ from logzero import logger
 
 data_dir = Path(__file__).with_name("data")
 try:
-    text1 = Path(data_dir, "test-en.txt").read_text()
+    text1 = Path(data_dir, "test-en.txt").read_text(encoding="utf8")
 except Exception as exc:
-    logger.eror("rea text1 error: %s, setting to ''", exc)
+    logger.error("rea text1 error: %s, setting to ''", exc)
     text1 = ""
 try:
     text2 = Path(data_dir, "test-zh.txt").read_text()
